efbe053d8473dd33f727b2ea3b86b41b0eaf835d
Nicolae Cudlenco <146981376+ncudlenco@users.noreply.github.com>
2024-10-08 11:28:01 +0200

    feat: implement aad auth request signing

--- R/sign_request.R
+++ R/sign_request.R
@@ -5,7 +5,10 @@ sign_sha256 <- function(string, key)
 
 sign_cosmos_request <- function(key, verb, resource_type, resource_link, date)
 {
-    if(key$type == "resource" || key$type == "aadToken")
+    if(key$type == "aadToken")
+        return(curl::curl_escape(sprintf("type=%s&ver=1.0&sig=%s", key$type, key$value)))
+    
+    if(key$type == "resource")
         return(curl::curl_escape(key$value))
 
     if(inherits(date, "POSIXt"))
@@ -20,6 +23,7 @@ sign_cosmos_request <- function(key, verb, resource_type, resource_link, date)
         sep="\n"
     )
     sig <- sign_sha256(string_to_sign, key$value)
+
     curl::curl_escape(sprintf("type=%s&ver=1.0&sig=%s", key$type, sig))
 }
 
